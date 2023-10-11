be9f9b23b16989f2e3903f99bc2babd01b92502f
Tùng Anh <tuganhhy@gmail.com>
2023-10-11 20:24:11 +0700

    anhtt

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,11 @@
-var vm = new Vue({
-    // các tùy chọn
-  })
+var vueInstance = new Vue({
+    el: '#app',
+    data: {
+        title: 'Toi la tung anh',
+        url:'https://github.com/',
+        target:'_blank'
+
+    },
+    methods: {
+    }
+  });
